d00f70961cb253582aed612757fddf6579b9f201
bojhakom-dev <74716542+bojhakom-dev@users.noreply.github.com>
2021-01-13 17:27:33 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(3000,"localhost");
+  await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
